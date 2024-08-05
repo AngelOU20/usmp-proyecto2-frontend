688c7362b8f8afcdc1edd61453620965ee36e339
Julio Ucharima <julio_ucharima@usmp.pe>
2024-08-05 11:10:46 -0500

    add layout for the pages

---
+++ app/(routes)/layout.tsx
@@ -0,0 +1,23 @@
+import { Navbar } from "@/components/navbar";
+import { Sidebar } from "@/components/sidebar";
+import React from "react";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Homelayout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex w-full h-full">
+      <div className="hidden lg:block w-80 h-full lg:fixed">
+        <Sidebar />
+      </div>
+      <div className="w-full lg:ml-80">
+        <Navbar />
+        <div className="p-6 bg-[#fafbfc] dark:bg-background">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Homelayout;
