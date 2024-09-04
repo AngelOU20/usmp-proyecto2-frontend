df614cabc1d11d94a1e1342e818eae11de7d9eb1
Julio Ucharima <julio_ucharima@usmp.pe>
2024-09-03 20:56:57 -0500

    add authentication check and redirect for protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,19 @@
-export { auth as middleware } from "@/lib/authOptions";
+// export { auth as middleware } from "@/lib/authOptions";
+
+import { auth } from "@/lib/authOptions";
+
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname !== "/sign-in") {
+    const newUrl = new URL("/sign-in", req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+});
+
+export const config = {
+  matcher: [
+    "/",             // Protege la ruta raíz
+    "/fileupload",   // Protege la ruta /fileupload
+    "/chat",         // Protege la ruta /chat
+    "/faqs",         // Protege la ruta /faqs
+  ]
+};
