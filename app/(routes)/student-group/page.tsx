7f72523c9858f68834b2bad1e39c41fb260bffe0
Julio Ucharima <julio_ucharima@usmp.pe>
2024-09-09 12:12:30 -0500

    add StudentsGroupPage with UserTable component

---
+++ app/(routes)/student-group/page.tsx
@@ -0,0 +1,11 @@
+import { UserTable } from "./components/user-table";
+
+export default function StudentsGroupPage() {
+  return (
+    <>
+      <h1>Grupo de estudiantes</h1>
+
+      <UserTable />
+    </>
+  );
+}
