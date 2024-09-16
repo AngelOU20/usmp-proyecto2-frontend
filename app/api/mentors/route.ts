fc3c8676ae57b182fa9e778ee8dcaf33e14549f7
Julio Ucharima <julio_ucharima@usmp.pe>
2024-09-16 13:46:08 -0500

    add mentor data retrieval API with user association

---
+++ app/api/mentors/route.ts
@@ -0,0 +1,22 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET (request: Request) {
+  const mentors = await prisma.mentor.findMany({
+    orderBy: {
+      createdAt: "asc",
+    },
+    include: {
+      user: true, // Incluye los datos de la tabla user relacionados con cada mentor
+    },
+  });
+
+  const mentorData = mentors.map((mentor) => ({
+    id: mentor.user.id,
+    name: mentor.user.name,
+    email: mentor.user.email,
+    phone: mentor.user.phone || "No disponible",
+  }));
+
+  return NextResponse.json(mentorData);
+}
