2a91ba0f9a4047baf950527695503d9e34cba7ba
Julio Ucharima <julio_ucharima@usmp.pe>
2024-09-17 16:30:46 -0500

    add mock mentor data for testing

---
+++ app/(routes)/mentor-management/components/mentor.data.ts
@@ -0,0 +1,29 @@
+import { User } from "./mentor.type";
+
+export const users: User[] = [
+  {
+    id: "1",
+    name: "Julio Ucharima",
+    email: "julio_ucharima@usmp.pe",
+  },
+  {
+    id: "2",
+    name: "Sandro Baldera",
+    email: "sandro_baldera@usmp.pe",
+  },
+  {
+    id: "3",
+    name: "Renato Valer",
+    email: "renato_valer@usmp.pe",
+  },
+  {
+    id: "4",
+    name: "Renato Valer",
+    email: "renato_valer@usmp.pe",
+  },
+  {
+    id: "5",
+    name: "Renato Valer",
+    email: "renato_valer@usmp.pe",
+  },
+];
