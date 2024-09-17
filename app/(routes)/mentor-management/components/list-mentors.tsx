645b088a1c6d4ce873667a2ac41c3c5c826fe48a
Julio Ucharima <julio_ucharima@usmp.pe>
2024-09-17 15:56:48 -0500

    add mentor listing with loading and error states

---
+++ app/(routes)/mentor-management/components/list-mentors.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { UserTable } from "./user-table";
+import { columns } from "./user-table-columns";
+import { Spinner } from "@/components/spinner";
+
+export const ListMentors = () => {
+  const [mentorData, setMentorData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // Estado para controlar la carga
+  const [error, setError] = useState<string | null>(null); // Estado para manejar errores (opcional)
+
+  useEffect(() => {
+    const fetchMentors = async () => {
+      setIsLoading(true); // Comienza la carga
+      try {
+        const response = await fetch("/api/mentors");
+        if (!response.ok) {
+          throw new Error("Error fetching mentors");
+        }
+        const data = await response.json();
+        setMentorData(data);
+        setIsLoading(false); // Finaliza la carga
+      } catch (error) {
+        setError("Error al cargar mentores"); // En caso de error
+        setIsLoading(false);
+      }
+    };
+
+    fetchMentors();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="w-full flex justify-center items-center">
+        <Spinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return <UserTable columns={columns} data={mentorData} />;
+};
