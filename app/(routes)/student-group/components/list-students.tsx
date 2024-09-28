5ca2a09bc426b0337efc1183515f7670c5b82083
Julio Ucharima <julio_ucharima@usmp.pe>
2024-09-28 15:44:18 -0500

    implement student deletion with refetch and toast notifications

--- app/(routes)/student-group/components/list-students.tsx
+++ app/(routes)/student-group/components/list-students.tsx
@@ -5,32 +5,61 @@ import { StudentTable } from "./student-table";
 import { Spinner } from "@/components/spinner";
 import { getColumns } from "./student-table-columns";
 import { Student } from "./student.type";
+import { toast } from "@/hooks/use-toast";
 
 export const ListStudents = () => {
   const [studentData, setStudentData] = useState<Student[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  const columns = React.useMemo(() => getColumns(), []);
+  const fetchStudents = async () => {
+    setIsLoading(true); // Comienza la carga
+    try {
+      const response = await fetch("/api/student");
+      if (!response.ok) {
+        throw new Error("Error fetching students");
+      }
+      const data = await response.json();
+      setStudentData(data.data);
+      setIsLoading(false); // Finaliza la carga
+    } catch (error) {
+      setError("Error al cargar estudiantes"); // En caso de error
+      setIsLoading(false);
+    }
+  };
+
+  // Función para eliminar estudiante
+  const handleDelete = async (id: string) => {
+    try {
+      const response = await fetch(`/api/student?id=${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error("Error al eliminar el estudiante");
+      }
+
+      // Actualizar la tabla de estudiantes localmente
+      await fetchStudents();
+
+      toast({
+        title: "Estudiante eliminado",
+        description: "El estudiante ha sido eliminado exitosamente.",
+        variant: "default",
+      });
+    } catch (error) {
+      console.error("Error:", error);
+      toast({
+        title: "Error",
+        description: "Hubo un error al intentar eliminar al estudiante.",
+        variant: "destructive",
+      });
+    }
+  };
+
+  const columns = React.useMemo(() => getColumns(handleDelete), []);
 
   useEffect(() => {
-    const fetchMentors = async () => {
-      setIsLoading(true); // Comienza la carga
-      try {
-        const response = await fetch("/api/student");
-        if (!response.ok) {
-          throw new Error("Error fetching mentors");
-        }
-        const data = await response.json();
-        setStudentData(data.data);
-        setIsLoading(false); // Finaliza la carga
-      } catch (error) {
-        setError("Error al cargar mentores"); // En caso de error
-        setIsLoading(false);
-      }
-    };
-
-    fetchMentors();
+    fetchStudents();
   }, []);
 
   if (isLoading) {
