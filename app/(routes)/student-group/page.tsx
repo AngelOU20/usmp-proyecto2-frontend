04978435c9d643f3377a47a097f0e221cccb6ee8
Julio Ucharima <julio_ucharima@usmp.pe>
2024-10-04 17:53:52 -0500

    update layout in StudentsGroupPage component

--- app/(routes)/student-group/page.tsx
+++ app/(routes)/student-group/page.tsx
@@ -3,7 +3,7 @@ import { ListStudents } from "./components/list-students";
 
 export default function StudentsGroupPage() {
   return (
-    <div className="max-w-6xl mx-auto flex flex-col flex-auto h-full">
+    <div className="max-w-7xl mx-auto flex flex-col flex-auto h-full">
       <h1 className="text-2xl font-semibold mb-5">Grupo de estudiantes</h1>
 
       <CsvUploaderStudent />
