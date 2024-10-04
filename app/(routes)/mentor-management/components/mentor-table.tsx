a3174dd0bea9ec103379b5a0189a495b3954ecff
Julio Ucharima <julio_ucharima@usmp.pe>
2024-10-04 17:30:29 -0500

    refactor mentor table component for cleaner layout and reusability

--- app/(routes)/mentor-management/components/mentor-table.tsx
+++ app/(routes)/mentor-management/components/mentor-table.tsx
@@ -6,7 +6,6 @@ import {
   ColumnFiltersState,
   SortingState,
   VisibilityState,
-  flexRender,
   getCoreRowModel,
   getFilteredRowModel,
   getPaginationRowModel,
@@ -14,18 +13,11 @@ import {
   useReactTable,
 } from "@tanstack/react-table";
 
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { DataTablePagination, DataTableToolbar } from "@/components/data-table";
+import { DataTableToolbar } from "@/components/data-table";
 import { SimplifiedUser } from "./mentor.type";
 import { ColumnDef } from "@tanstack/react-table";
 import { DataTableFilterField } from "@/types/data-table.types";
+import { DataTable } from "@/components/data-table/data-table";
 
 // Actualiza el UserTable para recibir `data` como prop
 interface UserTableProps {
@@ -70,57 +62,8 @@ export const MentorTable: React.FC<UserTableProps> = ({ columns, data }) => {
   });
 
   return (
-    <div className="w-full">
-      <div className="flex items-center gap-2 py-4">
-        <DataTableToolbar table={table} filterFields={filterFields} />
-      </div>
-      <div className="rounded-md border">
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                ))}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows.length ? (
-              table.getRowModel().rows.map((row) => (
-                <TableRow key={row.id}>
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id}>
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))
-            ) : (
-              <TableRow>
-                <TableCell
-                  colSpan={columns.length}
-                  className="h-24 text-center"
-                >
-                  No hay resultados.
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </div>
-
-      <DataTablePagination table={table} />
-    </div>
+    <DataTable table={table}>
+      <DataTableToolbar table={table} filterFields={filterFields} />
+    </DataTable>
   );
 };
