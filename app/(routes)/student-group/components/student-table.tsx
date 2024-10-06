ce3cc6bba0c320eb4a9f3dcabb4f8c32f31d4c62
Julio Ucharima <julio_ucharima@usmp.pe>
2024-10-05 19:31:03 -0500

    refactor: simplify student table filter generation using utility function

--- app/(routes)/student-group/components/student-table.tsx
+++ app/(routes)/student-group/components/student-table.tsx
@@ -17,33 +17,12 @@ import { DataTableFilterField } from "@/types/data-table.types";
 import { Student } from "./student.type";
 import { ColumnDef } from "@tanstack/react-table";
 import { DataTable } from "@/components/data-table/data-table";
+import { generateFilterOptions } from "@/lib/utils";
 
-// Interfaz para recibir columnas y datos
 interface UserTableProps {
   columns: ColumnDef<Student>[];
   data: Student[];
 }
-
-// Función genérica para obtener valores únicos y generar opciones de filtro
-const getUniqueValues = <T extends keyof Student>(
-  students: Student[],
-  key: T
-) => {
-  const values = students.map((student) => student[key]);
-  return Array.from(new Set(values)).filter((value) => value);
-};
-
-// Función genérica para generar opciones de filtro dinámicamente
-const generateFilterOptions = <T extends keyof Student>(
-  students: Student[],
-  key: T
-) => {
-  const uniqueValues = getUniqueValues(students, key);
-  return uniqueValues.map((value) => ({
-    label: String(value), // Convertir a string para usar como etiqueta en los filtros
-    value,
-  }));
-};
 
 export const StudentTable: React.FC<UserTableProps> = ({ columns, data }) => {
   const [sorting, setSorting] = React.useState<SortingState>([]);
@@ -54,7 +33,6 @@ export const StudentTable: React.FC<UserTableProps> = ({ columns, data }) => {
     React.useState<VisibilityState>({});
   const [rowSelection, setRowSelection] = React.useState({});
 
-  // Obtener las opciones de filtro para semestre, grupo y asignatura
   const semesterFilterOptions = generateFilterOptions(data, "semester");
   const groupFilterOptions = generateFilterOptions(data, "group");
   const subjectFilterOptions = generateFilterOptions(data, "subject");
@@ -66,11 +44,6 @@ export const StudentTable: React.FC<UserTableProps> = ({ columns, data }) => {
       placeholder: "Filtrar por nombre del alumno",
     },
     {
-      label: "Semestre",
-      value: "semester",
-      options: semesterFilterOptions,
-    },
-    {
       label: "Grupo",
       value: "group",
       options: groupFilterOptions,
@@ -79,6 +52,11 @@ export const StudentTable: React.FC<UserTableProps> = ({ columns, data }) => {
       label: "Asignatura",
       value: "subject",
       options: subjectFilterOptions,
+    },
+    {
+      label: "Semestre",
+      value: "semester",
+      options: semesterFilterOptions,
     },
   ];
 
