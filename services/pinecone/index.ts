c0c4e73ffe92defc5a62226d4ab3fe0e14fa567d
Julio Ucharima <julio_ucharima@usmp.pe>
2024-10-29 17:05:05 -0500

    add dynamic indexName support in vector-store functions

--- services/pinecone/index.ts
+++ services/pinecone/index.ts
@@ -4,14 +4,19 @@ import { getPineconeClient } from "../pinecone-client";
 import { embedAndStoreDocs } from "../vector-store";
 
 // Función para guardar en Pinecone
-export const saveOnPinecone = async (files: File[]) => {
+export const saveOnPinecone = async (files: File[], documentType: string) => {
   try {
     logger.info("Preparing chunks from PDF files");
     const docs = await getChunkedDocsFromUploadedPDFs(files);
 
-    logger.info(`Loading ${docs.length} chunks into pinecone...`);
+    const indexName = `${documentType}`;
 
-    await embedAndStoreDocs(docs);
+    // Obtener el cliente de Pinecone
+    await getPineconeClient(indexName);
+
+    logger.info(`Loading ${docs.length} chunks into pinecone...`);
+    await embedAndStoreDocs(docs, indexName);
+    // await embedAndStoreDocs(docs);
     logger.info("Data embedded and stored in pine-cone successfully.");
   } catch (error) {
     console.error("saveOnPinecone failed ", error);
@@ -19,21 +24,21 @@ export const saveOnPinecone = async (files: File[]) => {
 };
 
 // Función para eliminar el vector en Pinecone
-export async function deleteVectorFromPinecone (vectorId: string) {
-  if (!vectorId || vectorId.trim() === "") {
-    logger.error("El ID del vector no es válido.");
-    throw new Error("El ID del vector no es válido.");
-  }
+// export async function deleteVectorFromPinecone (vectorId: string) {
+//   if (!vectorId || vectorId.trim() === "") {
+//     logger.error("El ID del vector no es válido.");
+//     throw new Error("El ID del vector no es válido.");
+//   }
 
-  const pinecone = await getPineconeClient();
-  const index = pinecone.Index(process.env.PINECONE_INDEX_NAME!);
+//   const pinecone = await getPineconeClient();
+//   const index = pinecone.Index(process.env.PINECONE_INDEX_NAME!);
 
-  try {
-    // Eliminar el vector por ID utilizando deleteOne
-    await index.deleteOne(vectorId); // Pasa el ID directamente como cadena
-    logger.info("Vector eliminado de Pinecone:", vectorId);
-  } catch (error) {
-    logger.error(`Error al eliminar el vector de Pinecone (ID: ${vectorId}):`, error);
-    throw new Error(`Error al eliminar el vector de Pinecone con ID: ${vectorId}`);
-  }
-}
+//   try {
+//     // Eliminar el vector por ID utilizando deleteOne
+//     await index.deleteOne(vectorId); // Pasa el ID directamente como cadena
+//     logger.info("Vector eliminado de Pinecone:", vectorId);
+//   } catch (error) {
+//     logger.error(`Error al eliminar el vector de Pinecone (ID: ${vectorId}):`, error);
+//     throw new Error(`Error al eliminar el vector de Pinecone con ID: ${vectorId}`);
+//   }
+// }
